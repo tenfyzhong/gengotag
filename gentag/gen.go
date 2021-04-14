94518e4d10ecf93ad3c9bdd1effd96dedfae1933
tenfy <tenfy@tenfy.cn>
2021-04-14 19:38:44 +0800

    refactor

---
+++ gentag/gen.go
@@ -0,0 +1,103 @@
+package gentag
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+)
+
+func Gen(file, tagtype string, omitempty bool) (string, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "read file %v\n", err)
+		return "", err
+	}
+
+	v, err := unmarshalJson(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unmarshal json: %v\n", err)
+		return "", err
+	}
+
+	node := parseNode("", v)
+	root := node
+	for root.Type == TypeArray {
+		if len(root.Children) == 0 {
+			return "", nil
+		}
+		root = root.Children[0]
+	}
+
+	gen := NewStructGenerator()
+	gen.Generate(root)
+
+	text := render(gen, tagtype, omitempty)
+	return text, nil
+}
+
+func unmarshalJson(data []byte) (interface{}, error) {
+	var v interface{}
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+func parseNode(tag string, v interface{}) *Node {
+	node := &Node{
+		Tag: tag,
+	}
+	switch c := v.(type) {
+	case bool:
+		node.Type = TypeBool
+	case float64:
+		node.Type = TypeFloat64
+	case string:
+		node.Type = TypeString
+	case map[string]interface{}:
+		node.Type = TypeStruct
+		node.Children = NodeList{}
+		for k, v := range c {
+			child := parseNode(k, v)
+			if child == nil {
+				continue
+			}
+			node.Children = append(node.Children, child)
+		}
+		sort.Sort(node.Children)
+	case []interface{}:
+		node.Type = TypeArray
+		if len(c) > 0 {
+			child := parseNode("", c[0])
+			node.Children = append(node.Children, child)
+		}
+	}
+	node.fingerprint = fingerprint(node)
+	return node
+}
+
+func render(gen *StructGenerator, tagtype string, omitempty bool) string {
+	result := ""
+	omitemptyStr := ""
+	if omitempty {
+		omitemptyStr = ",omitempty"
+	}
+
+	structList := StructList{}
+	for _, st := range gen.m {
+		structList = append(structList, st)
+	}
+	sort.Sort(structList)
+
+	for _, st := range structList {
+		result += fmt.Sprintf("type %s struct {\n", st.Name)
+		for _, field := range st.Field {
+			result += fmt.Sprintf("\t%s %s `%s:\"%s%s\"`\n", tag2Name(field.Tag), field.TypeName, tagtype, field.Tag, omitemptyStr)
+		}
+		result += "}\n\n"
+	}
+	return result
+}
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"sort"
+
+	"github.com/tenfyzhong/gengotag/gentag"
 )
 
 var (
@@ -27,96 +26,10 @@ func init() {
 }
 
 func main() {
-	data, err := ioutil.ReadFile(file)
+	text, err := gentag.Gen(file, tagtype, omitempty)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "read file %v\n", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(2)
 	}
-
-	v, err := unmarshalJson(data)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unmarshal json: %v\n", err)
-		os.Exit(3)
-	}
-
-	node := parseNode("", v)
-	root := node
-	for root.Type == TypeArray {
-		if len(root.Children) == 0 {
-			os.Exit(0)
-		}
-		root = root.Children[0]
-	}
-
-	gen := NewStructGenerator()
-	gen.Generate(root)
-
-	text := render(gen)
 	fmt.Printf("%s", text)
-
-}
-
-func unmarshalJson(data []byte) (interface{}, error) {
-	var v interface{}
-	err := json.Unmarshal(data, &v)
-	if err != nil {
-		return nil, err
-	}
-	return v, nil
-}
-
-func parseNode(tag string, v interface{}) *Node {
-	node := &Node{
-		Tag: tag,
-	}
-	switch c := v.(type) {
-	case bool:
-		node.Type = TypeBool
-	case float64:
-		node.Type = TypeFloat64
-	case string:
-		node.Type = TypeString
-	case map[string]interface{}:
-		node.Type = TypeStruct
-		node.Children = NodeList{}
-		for k, v := range c {
-			child := parseNode(k, v)
-			if child == nil {
-				continue
-			}
-			node.Children = append(node.Children, child)
-		}
-		sort.Sort(node.Children)
-	case []interface{}:
-		node.Type = TypeArray
-		if len(c) > 0 {
-			child := parseNode("", c[0])
-			node.Children = append(node.Children, child)
-		}
-	}
-	node.fingerprint = fingerprint(node)
-	return node
-}
-
-func render(gen *StructGenerator) string {
-	result := ""
-	omitemptyStr := ""
-	if omitempty {
-		omitemptyStr = ",omitempty"
-	}
-
-	structList := StructList{}
-	for _, st := range gen.m {
-		structList = append(structList, st)
-	}
-	sort.Sort(structList)
-
-	for _, st := range structList {
-		result += fmt.Sprintf("type %s struct {\n", st.Name)
-		for _, field := range st.Field {
-			result += fmt.Sprintf("\t%s %s `%s:\"%s%s\"`\n", tag2Name(field.Tag), field.TypeName, tagtype, field.Tag, omitemptyStr)
-		}
-		result += "}\n\n"
-	}
-	return result
 }
