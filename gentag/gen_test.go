d9aa4e4c9f6230d762dc2eebb672e89f9a47cbec
tenfy <tenfy@tenfy.cn>
2021-04-14 20:11:18 +0800

    unittest

---
+++ gentag/gen_test.go
@@ -0,0 +1,135 @@
+package gentag
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGen(t *testing.T) {
+	type args struct {
+		data      []byte
+		tagtype   string
+		omitempty bool
+	}
+	tests := []struct {
+		name string
+		args func(t *testing.T) args
+
+		want1      string
+		wantErr    bool
+		inspectErr func(err error, t *testing.T) //use for more precise error evaluation after test
+	}{
+		{
+			name: "empty",
+			args: func(t *testing.T) args {
+				return args{
+					data:      []byte{},
+					tagtype:   "json",
+					omitempty: false,
+				}
+			},
+			want1:   "",
+			wantErr: true,
+			inspectErr: func(err error, t *testing.T) {
+				if err.Error() != "unexpected end of JSON input" {
+					t.FailNow()
+				}
+			},
+		},
+		{
+			name: "empty struct",
+			args: func(t *testing.T) args {
+				return args{
+					data:      []byte(`{}`),
+					tagtype:   "json",
+					omitempty: false,
+				}
+			},
+			want1:   "type Struct struct {\n}\n\n",
+			wantErr: false,
+		},
+		{
+			name: "empty array",
+			args: func(t *testing.T) args {
+				return args{
+					data:      []byte(`[]`),
+					tagtype:   "json",
+					omitempty: false,
+				}
+			},
+			want1:   "",
+			wantErr: false,
+		},
+		{
+			name: "num array",
+			args: func(t *testing.T) args {
+				return args{
+					data:      []byte(`[1, 2]`),
+					tagtype:   "json",
+					omitempty: false,
+				}
+			},
+			want1:   "",
+			wantErr: false,
+		},
+		{
+			name: "struct1",
+			args: func(t *testing.T) args {
+				return args{
+					data:      []byte(`{"hello_world": 1}`),
+					tagtype:   "json",
+					omitempty: true,
+				}
+			},
+			want1:   "type Struct struct {\n\tHelloWorld float64 `json:\"hello_world,omitempty\"`\n}\n\n",
+			wantErr: false,
+		},
+		{
+			name: "struct2",
+			args: func(t *testing.T) args {
+				return args{
+					data: []byte(`
+{
+    "hello": {
+        "aaa1": 111,
+        "bbb2": 222,
+        "ccc3": 333
+    },
+    "world": {
+        "aaa1": 0,
+        "bbb2": 1,
+        "ccc3": 2
+    },
+	"a_bool": true,
+	"hello_world": "hello_world"
+}
+`),
+					tagtype:   "json",
+					omitempty: false,
+				}
+			},
+			want1:   "type Hello struct {\n\tAaa1 float64 `json:\"aaa1\"`\n\tBbb2 float64 `json:\"bbb2\"`\n\tCcc3 float64 `json:\"ccc3\"`\n}\n\ntype Struct struct {\n\tABool bool `json:\"a_bool\"`\n\tHello *Hello `json:\"hello\"`\n\tHelloWorld string `json:\"hello_world\"`\n\tWorld *Hello `json:\"world\"`\n}\n\n",
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tArgs := tt.args(t)
+
+			got1, err := Gen(tArgs.data, tArgs.tagtype, tArgs.omitempty)
+
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("Gen got1 = `%v`, want1: `%v`", got1, tt.want1)
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("Gen error = %v, wantErr: %t", err, tt.wantErr)
+			}
+
+			if tt.inspectErr != nil {
+				tt.inspectErr(err, t)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/tenfyzhong/gengotag
 
 go 1.12
-
-require (
-	github.com/golang/mock v1.5.0
-	github.com/stretchr/testify v1.7.0
-)
